11ee110e82e3ef0f9854e3ce6307dde47e22b0f7
Alex Sobolev <31108@mail.ru>
2024-04-28 17:12:47 +0300

    Initial Commit

---
+++ AnimationAppSwiftUI/AnimationAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationAppSwiftUIApp.swift
+//  AnimationAppSwiftUI
+//
+//  Created by Александр Соболев on 28.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
